d4aca5897b10238257f8b8f7ae97caae4fa17b7a
chc <106435999+hsiu-chan@users.noreply.github.com>
2023-02-24 21:13:46 +0800

    modified hw1_binary_hex_converter and hw1_binary_hex_converter.cpp in .

--- hw1_binary_hex_converter.cpp
+++ hw1_binary_hex_converter.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 
-void output(vector<int> arr){
-    map<int,char> to_hex={{10,'A'},{11,'B'},{12,'C'},{13,'D'},{14,'E'},{15,'F'}};
+void output(vector<int> arr){//輸出函數
+    map<int,char> to_hex={{10,'A'},{11,'B'},{12,'C'},{13,'D'},{14,'E'},{15,'F'}};//超過10轉ABCEDF
+    
     for(int i=arr.size();i>0;i--){
         arr[i-1]>9 ? cout<<to_hex[arr[i-1]]:cout<<arr[i-1];
     }
@@ -16,27 +17,28 @@ void output(vector<int> arr){
 int main(){
     int input;
     cin>>input;
-    if (input==0){
+    if (input==0){//特判0
         cout<<"0\n0";
         return 0;
     }
 
-    vector<int> bi_format;
+
+
     int input_copy1=input;
+    vector<int> bi_format;//二進制
     while (input_copy1>0)
     {
         bi_format.push_back(input_copy1%2);
-        input_copy1>>=1;
+        input_copy1>>=1;//input除2
     }
     output(bi_format);
 
-
     int input_copy2=input;
-    vector<int> hex_format;
+    vector<int> hex_format;//十六進制
     while (input_copy2>0)
     {
         hex_format.push_back(input_copy2%16);
-        input_copy2>>=4;
+        input_copy2>>=4;//input除16
     }
     output(hex_format);
 }
